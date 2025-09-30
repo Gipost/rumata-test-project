c049bd344f39cfbb9e0bec357877e2603cacaf67
Vadim Barynkin <dmibarhq432@gmail.com>
2025-09-30 15:45:42 +0300

    Patrolling option in base behaviour

--- game/objects/entities/enemy/enemy_behaviour/chaser_behaviour.gd
+++ game/objects/entities/enemy/enemy_behaviour/chaser_behaviour.gd
@@ -1,7 +1,7 @@
 # ChaserBehavior.gd
 extends EnemyBehaviour
 class_name ChaserBehaviour
-#скорость преследования
+##скорость преследования
 @export var speed: float = 60.0
 ##использование дешей врагом
 @export var use_charging: bool = false
@@ -14,16 +14,10 @@ class_name ChaserBehaviour
 
 var charging := false
 var can_charge := true
-var vision_area: Area2D
-var detected_player: Node2D = null
 
-func setup(e):
-	enemy = e
-	vision_area = enemy.get_node("VisionArea")
-	vision_area.body_entered.connect(_on_body_entered)
-	vision_area.body_exited.connect(_on_body_exited)
 
-func update(delta):
+
+func update_behaviour(delta):
 	if enemy:
 		if not detected_player:
 			enemy.velocity = Vector2.ZERO
@@ -62,30 +56,16 @@ func _start_charge(enemy):
 		enemy.move_and_slide()
 
 	# dash lasts short burst
-		await enemy.get_tree().create_timer(0.3).timeout
-		enemy.velocity = Vector2.ZERO
-		charging = false
+	await enemy.get_tree().create_timer(0.3).timeout
+	enemy.velocity = Vector2.ZERO
+	charging = false
 
-		# cooldown before next charge
-		await enemy.get_tree().create_timer(charge_cooldown).timeout
-		can_charge = true
-
-
-func _on_body_entered(body: Node2D):
-	if body.is_in_group("player"):
-		detected_player = body
-
-func _on_body_exited(body: Node2D):
-	if body == detected_player:
-		detected_player = null
+	# cooldown before next charge
+	await enemy.get_tree().create_timer(charge_cooldown).timeout
+	can_charge = true
 
 
-func cleanup():
-	#очистка, чтобы таймеры не коллили уже удаленный instance
-	if is_instance_valid(vision_area):
-		if vision_area.body_entered.is_connected(_on_body_entered):
-			vision_area.body_entered.disconnect(_on_body_entered)
-		if vision_area.body_exited.is_connected(_on_body_exited):
-			vision_area.body_exited.disconnect(_on_body_exited)
-	detected_player = null
+
+func cleanup_additional():
+
 	charging = false
--- game/objects/entities/enemy/enemy_behaviour/enemy_behaviour.gd
+++ game/objects/entities/enemy/enemy_behaviour/enemy_behaviour.gd
@@ -3,9 +3,77 @@ extends Resource
 class_name EnemyBehaviour
 
 var enemy
+@export var patrolling: bool = false
+@export var patrol_distance: int = 64
+@export var patrol_speed: float = 60.0
+var _patrol_points: Array[Vector2] = []
+var _current_patrol_index := 0
+var _patrol_origin: Vector2
+
+var vision_area: Area2D
+var detected_player: Node2D = null
 
 func setup(e):
 	enemy = e
+	vision_area = enemy.get_node("VisionArea")
+	vision_area.body_entered.connect(_on_body_entered)
+	vision_area.body_exited.connect(_on_body_exited)
+	if patrolling:
+		_patrol_origin = enemy.global_position
+		_patrol_points = [
+			_patrol_origin + Vector2(patrol_distance, 0),   # right
+			_patrol_origin,                                # back left
+			_patrol_origin + Vector2(0, -patrol_distance), # up
+			_patrol_origin,                                # back down
+		]
 
-func update(delta): pass
-func cleanup(): pass
+func update(delta): 
+	if detected_player == null:
+		update_patrol(delta)
+	update_behaviour(delta)
+
+func cleanup(): 
+	if is_instance_valid(vision_area):
+		if vision_area.body_entered.is_connected(_on_body_entered):
+			vision_area.body_entered.disconnect(_on_body_entered)
+		if vision_area.body_exited.is_connected(_on_body_exited):
+			vision_area.body_exited.disconnect(_on_body_exited)
+	detected_player = null
+	cleanup_additional()
+
+func cleanup_additional(): pass
+
+func update_behaviour(delta): pass
+
+
+func update_patrol(delta) -> void:
+	
+	if _patrol_points.is_empty():
+		return
+
+	var target = _patrol_points[_current_patrol_index]
+	var dir = (target - enemy.global_position).normalized()
+	enemy.velocity = dir * patrol_speed
+	enemy.move_and_slide()
+
+	# При колизии со стеной скип до след точки в пути
+	for i in range(enemy.get_slide_collision_count()):
+		var collision = enemy.get_slide_collision(i)
+		if collision.get_collider() is TileMapLayer or collision.get_collider() is StaticBody2D:
+			_switch_to_next_patrol_point()
+			return
+
+	if enemy.global_position.distance_to(target) < 4.0:
+		_current_patrol_index = (_current_patrol_index + 1) % _patrol_points.size()
+
+#helper
+func _switch_to_next_patrol_point() -> void:
+	_current_patrol_index = (_current_patrol_index + 1) % _patrol_points.size()
+
+func _on_body_entered(body: Node2D):
+	if body.is_in_group("player"):
+		detected_player = body
+
+func _on_body_exited(body: Node2D):
+	if body == detected_player:
+		detected_player = null
