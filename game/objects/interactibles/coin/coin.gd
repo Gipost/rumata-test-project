d11b4914e707829ed64639722d408dbe9092c083
Vadim Barynkin <dmibarhq432@gmail.com>
2025-10-01 12:40:50 +0300

    simple coin magnet using tween

--- game/objects/interactibles/coin/coin.gd
+++ game/objects/interactibles/coin/coin.gd
@@ -3,9 +3,21 @@ extends Interactible
 
 func item_pickUP(body: Node2D) -> void:
 	if body is Player:
+		if has_node("PickUpRange"):
+			$PickUpRange.monitoring = false
+
+		# tween to player
+		var tween := create_tween()
+		tween.tween_property(self, "global_position", body.global_position, 0.3).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+
+		# кд перед исчезновением
+		await get_tree().create_timer(0.2).timeout
+
 		Globals.GameController.collected_coins += 1
 		Globals.GameController.emit_signal("coin_collected")
 		body.coin_sfx()
+
 		if Globals.GameController.collected_coins == Globals.GameController.total_coins:
 			Globals.GameController.start_victory_cutscene()
-		self.queue_free()
+
+		queue_free()
